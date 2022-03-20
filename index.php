003cfc36e5ba736adcf941280338b5eee140eab8
TrimuliaSarii <101724844+TrimuliaSarii@users.noreply.github.com>
2022-03-20 19:38:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,154 +1,155 @@
+<?php
+
 class MahlukHidup {
 
-function __construct(){
-    echo "ini mahluk hidup";
-    echo "<br>";
-public $nama; 
-
-function __construct($nama) {
-  $this->nama = $nama;
-}
-public function ambilNama()
-{
-  return $this->nama;
-}
-/
+    public $nama;
+  
+    function __construct($nama) {
+      $this->nama = $nama;
+    }
+    public function ambilNama()
+    {
+      return $this->nama;
+    }
 }
 
 class Manusia extends Mahlukhidup{
 
-public function Biodata($nama, $umur, $jeniskelamin)
-public function biodata($umur, $jeniskelamin)
-{
-    echo $nama . " - ";
-    echo $umur . " - ";
-    echo $jeniskelamin ."<br>";
-    echo "
-    MANUSIA <br>
-    # $this->nama <br>
-    - $umur Tahun<br>
-    - $jeniskelamin <br>
-    - Makanan Kesukaan <br>";
-
-}    
-public function makananKesukaan($makanan)
-{
-    echo "   
-    &nbsp;&nbsp; + $makanan
-    <br>
-    ";
-}    
+    public function biodata($umur, $jeniskelamin)
+    {
+        echo "
+        MANUSIA <br>
+        # $this->nama <br>
+        - $umur Tahun<br>
+        - $jeniskelamin <br>
+        - Makanan Kesukaan <br>";
+        
+    }    
+    public function makananKesukaan($makanan)
+    {
+        echo "
+        &nbsp;&nbsp; + $makanan
+        <br>
+        ";
+    }    
 }
-class Binatang extends Mahlukhidup{
 class Hewan extends Mahlukhidup{
 
-public function Hewan($nama, $jenis, $kelamin)
-public function biodataHewan($umur, $jenishewan ,$jeniskelamin)
-{
-    echo $nama . " - ";
-    echo $jenis . " - ";
-    echo $kelamin ."<br>";
-    echo "
-    HEWAN <br>
-    # $this->nama <br>
-    - $umur Bulan <br>
-    - $jenishewan <br>
-    - $jeniskelamin <br>
-    - Makanan Kesukaan Hewan <br>";
-
-}    
-public function makananKesukaanHewan($makanan)
-{
-    echo "
-    &nbsp;&nbsp; + $makanan
-    <br>
-    ";
-}    
+    public function biodataHewan($umur, $jenishewan ,$jeniskelamin)
+    {
+        echo "
+        HEWAN <br>
+        # $this->nama <br>
+        - $umur Tahun <br>
+        - $jenishewan <br>
+        - $jeniskelamin <br>
+        - Makanan Kesukaan Hewan <br>";
+        
+    }    
+    public function makananKesukaanHewan($makanan)
+    {
+        echo "
+        &nbsp;&nbsp; + $makanan
+        <br>
+        ";
+    }    
 }
 
-class BendaMati{
-public $namabenda;
 
-function __construct($namabenda) {
-  $this->namabenda = $namabenda;
-}
-public function ambilNamabenda()
-{
-  return $this->namabenda;
-}
-}
 
-$new = New Manusia;
-$aa = New Binatang;
-$new->Biodata("Usman Habib", 12, "L");
-$aa->Hewan("Usman Habib", 12, "L");
-class Kendaraan extends BendaMati{
-public function jenisKendaraan($merek, $tipe ,$jenis)
-{
-    echo "
-    Kendaraan <br>
-    # $this->namabenda <br>
-    - $merek <br>
-    - $tipe <br>
-    - $jenis <br>
-    - Detail Kendaraan <br>";
+////////////////////////////////
 
-}    
-public function banyakKendaraan($warna, $jumlah)
-{
-    echo "
-    &nbsp;&nbsp; + $warna = jumlah $jumlah
-    <br>
-    ";
-}    
+
+class Bangunan {
+    public $nama;
+  
+    function __construct($nama) {
+      $this->nama = $nama;
+    }
+    public function ambilNama()
+    {
+      return $this->nama;
+    }
 }
-class Lainnya extends BendaMati{
-public function jenisbenda($merek, $tipe ,$jenis)
-{
-    echo "
-    Benda Lainnya <br>
-    # $this->namabenda <br>
-    - $merek <br>
-    - $tipe <br>
-    - $jenis <br>
-    - Detail Benda <br>";
-
-}    
-public function banyakbenda($warna, $jumlah)
-{
-    echo "
-    &nbsp;&nbsp; + $warna = jumlah $jumlah unit
-    <br>
-    ";
-}    
+ 
+class Pribadi extends Bangunan {
+    public function detailRuangan($luas, $harga)
+    {
+        echo "
+        Bangunan Pribadi <br>
+        # $this->nama <br>
+        - Luas Tanah $luas <br>
+        - Harga $harga <br>
+        - Jumlah Ruangan <br>";
+        
+    }    
+    public function jumlahRuangan($nama_ruangan, $jumlah)
+    {
+        echo "
+        &nbsp;&nbsp; + $nama_ruangan $jumlah ruangan.
+        <br>
+        ";
+    }    
+    
 }
+class Umum extends Bangunan {
+    public function detailBangunan($luas, $harga)
+    {
+        echo "
+        Bangunan Umum <br>
+        # $this->nama <br>
+        - Luas Tanah $luas <br>
+        - Harga $harga <br>
+        - Jumlah Bangunan Ruangan <br>";
+        
+    }    
+    public function jumlahBangunan($nama_bangunan, $jumlah)
+    {
+        echo "
+        &nbsp;&nbsp; + $nama_bangunan $jumlah Ruang.
+        <br>
+        ";
+    }   
 
+}
 
 echo "++CLASS PARENT PERTAMA++ <br>";
 echo "==CLASS CHILD PERTAMA==<br><br>";
-$new = New Manusia("Usman");
-$new->biodata(19, "Laki Laki");
-$new->makananKesukaan("Pempek");
-$new->makananKesukaan("Siomay");
-$new->makananKesukaan("Ayam Goyeng");
+$new = New Manusia("Tri Mulia");
+$new->biodata(19, "Wanita");
+$new->makananKesukaan("Bakso");
+$new->makananKesukaan("Mie Ayam");
 echo "<br>==CLASS CHILD KEDUA==<br><br>";
-$hewan = New Hewan("Kucing");
-$hewan->biodataHewan(19, "Mamalia" ,"Laki Laki");
-$hewan->makananKesukaanHewan("Ikan Asin");
-$hewan->makananKesukaanHewan("Wishkas");
-$hewan->makananKesukaanHewan("Ayam Goyeng");
+$hewan = New Hewan("Harimau");
+$hewan->biodataHewan(2, "Mamalia" ,"Betina");
+$hewan->makananKesukaanHewan("Daging Ayam");
+$hewan->makananKesukaanHewan("Ceker Ayam");
 
 
-echo "<br><br><br>++CLASS PARENT KEDUA++ <br>";
+echo  
+"<br><br>";
+
+echo "++CLASS PARENT PERTAMA++ <br>";
 echo "==CLASS CHILD PERTAMA==<br><br>";
-$benda1 = New Kendaraan("Mobil");
-$benda1->jenisKendaraan("HONDA", "Matic", "SPORT");
-$benda1->banyakKendaraan("Merah", 10);
-$benda1->banyakKendaraan("Hijau", 190);
-$benda1->banyakKendaraan("Biru", 17);
-echo "<br>==CLASS CHILD KEDUA==<br><br>";
-$benda1 = New Kendaraan("Kursi");
-$benda1->jenisKendaraan("INOAC", "Kursi kantor", "tipe kursi singel");
-$benda1->banyakKendaraan("Kuning", 90);
-$benda1->banyakKendaraan("Hijau", 190);
-$benda1->banyakKendaraan("Hitam", 177);
+
+$pribadi = new Pribadi("Rumah Pribadi");
+$pribadi->detailRuangan("500m2", "Rp.80000000");
+$pribadi->jumlahRuangan("kamar tidur", 3);
+$pribadi->jumlahRuangan("Dapur ", 1);
+$pribadi->jumlahRuangan("Kamar Mandi", 2);
+
+
+echo "==CLASS CHILD KEDUA ==<br><br>";
+
+$pribadi = new Umum("Kampus");
+$pribadi->detailBangunan("800m2", "Rp.100000000");
+$pribadi->jumlahBangunan("Ruang Mahasiswa", 25);
+$pribadi->jumlahBangunan("Ruang Dosen", 4);
+$pribadi->jumlahBangunan("Ruang Audit", 1    );
+
+
+
+
+
+
+
